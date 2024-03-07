690ab03b0fbeeb8f3f0a69c9341fdc63e9a779d8
Aurel <157985436+GAurel396@users.noreply.github.com>
2024-03-07 17:06:47 +0100

    added function for one round. bugpresent  in player input checker and the choices if else

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-console.log ("Choose rock, paper, or scissor");
 function ComputerChoice () {
-    const CompPossible = ["rock", "paper", "scissor"];
+    const CompPossible = ["rock", "paper", "scissors"];
     let CompPick = Math.floor(Math.random() * 3);
     return CompPossible [CompPick];
     }
+function PlayRound (ComputerMove, PlayerMove ) {
+    if (PlayerMove === ComputerMove) {
+        console.log ("It's a tie!")
+    };
 
-    let ComputerMove=ComputerChoice();
-    console.log(ComputerMove);
+    if (PlayerChoice="rock") {
+        if (ComputerMove="paper") {
+            console.log ("You lost! Paper beats rock!")
+        } else if (ComputerMove="scissors") {
+            console.log("You won! Rock beats scissors!")
+        }
+    }
+    
+    if (PlayerChoice="paper") {
+        if (ComputerMove="scissors") {
+            console.log ("You lost! Scissors beat paper!")
+        } else if (ComputerMove="rock") {
+            console.log ("You won! Paper beats rock!")
+        } 
+    }
+
+    if(PlayerChoice="scissors") {
+        if (ComputerMove="rock") {
+            console.log ("You lost! Rock beats scissors!")
+        } else if (ComputerMove ="paper") {
+            console.log ("You won! Scissors beat paper!")
+        }
+
+    }
+}
+
+let ComputerMove=ComputerChoice();
+let PlayerMove = prompt("Choose your move:");   
+PlayerMove = PlayerMove.toLowerCase();
+if (PlayerMove!=="rock"&& PlayerMove!=="paper"&& PlayerMove!=="scissors") {
+    console.log("Stop messing around, put in valid choice.");
+    let PlayerChoice = prompt("Choose your move:");
+PlayRound(ComputerChoice, PlayerMove)    
+}
