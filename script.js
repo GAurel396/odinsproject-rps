04a2779afb6474d7efceccbbff9ec1aa95acb1ef
Aurel <157985436+GAurel396@users.noreply.github.com>
2024-03-07 11:28:46 +0100

    Added computer move function

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-console.log ("Choose rock (R), paper (P), or scissor (S)");
-//take input from the user, save it to  variable
+console.log ("Choose rock, paper, or scissor");
+function ComputerChoice () {
+    const CompPossible = ["rock", "paper", "scissor"];
+    let CompPick = Math.floor(Math.random() * 3);
+    return CompPossible [CompPick];
+    }
+
+    let ComputerMove=ComputerChoice();
+    console.log(ComputerMove);
